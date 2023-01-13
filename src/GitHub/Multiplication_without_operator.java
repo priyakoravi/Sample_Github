a2bbdafb4a697b5f58c850ba523dc139749068d5
Rohit <Rohit@LAPTOP-C4EBIEEK>
2023-01-13 21:43:23 +0530

    Hii this is priyanka

---
+++ src/GitHub/Multiplication_without_operator.java
@@ -0,0 +1,20 @@
+package GitHub;
+
+public class Multiplication_without_operator
+{
+	public static void main(String[] args) 
+	{
+		int num1=10;
+		int num2=20;
+		
+		int sum=0;
+		
+		for(int i=1; i<=num1;i++)
+		{
+			sum=sum+num2;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
